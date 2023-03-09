0bcb10b34ad9ba0ef0169e498cd4de7a1bfb08d5
DJFancyAl <abluemleiv@gmail.com>
2023-03-08 21:19:20 -0500

    Created a wildcard route.

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+// Home Page
 app.get('/', (req, res) => {
     res.send('Hello World...')
 })
 
+// Wildcard Route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+// Server Listener
 app.listen(process.env.PORT, () => {
     console.log('Rest-Rant Server Running...')
 })

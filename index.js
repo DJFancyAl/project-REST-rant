7967916d66edb2cfe4c4cc134df05694358873b4
DJFancyAl <abluemleiv@gmail.com>
2023-03-11 11:36:05 -0500

    Added home view and places/index view, which renders a summary of each place.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 require('dotenv').config()
 
-
 const express = require('express');
 const app = express();
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // Import Controllers
 app.use('/places', require('./controllers/places'))
 
 // Home Page
 app.get('/', (req, res) => {
-    res.send('Hello World...')
+    res.render('home')
 })
 
 // Wildcard Route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render('error404')
 })
 
 

286fae95ad44365bac6bb7985db74c65e0e5749a
DJFancyAl <abluemleiv@gmail.com>
2023-03-20 21:10:02 -0400

    Stubbed Out all Routes

--- controllers/places.js
+++ controllers/places.js
@@ -15,28 +15,59 @@ let places = [{
     pic: 'http://placekitten.com/250/250'
   }]
 
-// More code here in a moment
+// Places index page
 router.get('/', (req, res) => {      
     res.render('places/index', { places })
 })
 
-// Create New Place
+// Create new place
+router.post('/', (req, res) => {      
+    res.send("Create a new place.")
+})
+
+// Form page for creating a new place
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-// Show Place
-router.get('/:place', (req, res) => {
-    let id = req.params.place
+// Details about a particular place
+router.get('/:id', (req, res) => {
+    let id = req.params.id
     let place = places[id];
     res.render('places/show', {place, id})
 })
 
-// Edit Place
-router.get('/:place/edit', (req, res) => {
-    let id = req.params.place
+// Update a particular place
+router.put('/:id', (req, res) => {      
+    res.send("Update a particular place.")
+})
+
+// Form page for editing an existing place
+router.get('/:id/edit', (req, res) => {
+    let id = req.params.id
     let place = places[id];
     res.render('places/edit', {place, id})
+})
+
+// Delete a particular place
+router.delete('/:id', (req, res) => {      
+    res.send("Delete a particular place.")
+})
+
+// Create a rant (comment) about a particular place
+router.post('/:id/rant', (req, res) => {      
+    res.send("Create a rant (comment) about a particular place")
+})
+
+// Delete a rant (comment) about a particular place
+router.delete('/:id/rant/:id', (req, res) => {      
+    res.send("Delete a rant (comment) about a particular place")
+})
+
+
+// Wildcard Route
+router.get('*', (req, res) => {
+    res.status(404).render('error404')
 })
 
 module.exports = router

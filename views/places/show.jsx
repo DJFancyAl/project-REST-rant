6bb33bf67358d8d674d0b120f6688ccab4d1d482
DJFancyAl <abluemleiv@gmail.com>
2023-03-24 11:10:02 -0400

    Styled the Show Page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,11 +4,25 @@ const Def = require('../layouts/default')
 function show({place, id}){
     return (
         <Def>
-            <main>
-                <h1>{place.name}</h1>
-                <p>{place.city}, {place.state}</p>
-                <p>{place.cuisines}</p>
-                <p><a href={"/places/" + id + "/edit"}>Edit</a></p>
+            <main className='container'>
+                <h1 className='shadow-sm'>{place.name}</h1>
+                <div className='flex row align-items-stretch'>
+                    <div className='col-6'>
+                        <img className='img-fluid shadow' src={place.pic} alt={place.name} />
+                    </div>
+                    <div className='col-6 align-self-center text-center'>
+                        <h2>Rating</h2>
+                        <p>Not yet rated...</p>
+                        <h2 className='mt-5'>Description</h2>
+                        <h5>Located in {place.city}, {place.state}</h5>
+
+                        <p>{place.cuisines.map((cuisine) => {
+                            return cuisine + " * "
+                        })}
+                        </p>
+                        <a href={"/places/" + id + "/edit"}><button>Edit Place</button></a>
+                    </div>
+                </div>
             </main>
         </Def>
     )

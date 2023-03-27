cd8b3b93851fdfc06e8042b5f1bd11e727bb33bf
DJFancyAl <abluemleiv@gmail.com>
2023-03-27 14:04:25 -0400

    Corrected issue with Edit form by using defaultValue instead of value

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -12,19 +12,19 @@ function edit_form( {place, id} ){
                     <form method='POST' action={`/places/${id}?_method=PUT`}>
                         <div className='form-group mb-3'>
                             <label htmlFor="name">Name of Place</label>
-                            <input className='form-control' type="text" placeholder='The place...' name='name' value={place.name} required />
+                            <input className='form-control' type="text" placeholder='The place...' name='name' defaultValue={place.name} required />
                         </div>
                         <div className='form-group mb-3'>
                             <label htmlFor="pic">Picture of Place</label>
-                            <input className='form-control' type="url" id="pic" name="pic" value={place.pic} />
+                            <input className='form-control' type="url" id="pic" name="pic" defaultValue={place.pic} />
                         </div>
                         <div className='form-group mb-3'>
                             <label htmlFor="city">City</label>
-                            <input className='form-control' type="text" placeholder='City Name...' name='city' value={place.city} />
+                            <input className='form-control' type="text" placeholder='City Name...' name='city' defaultValue={place.city} />
                         </div>
                         <div className='form-group mb-1'>
                             <label htmlFor="state">State</label>
-                            <select className='form-control' name="state" id="state" value={place.state}>
+                            <select className='form-control' name="state" id="state" defaultValue={place.state}>
                                 <option value="" disabled selected>--State--</option>
                                 <option value="AL">Alabama</option>
                                 <option value="AK">Alaska</option>
@@ -84,25 +84,25 @@ function edit_form( {place, id} ){
                             <div className="row m-auto">
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("American") ?
-                                    <input type="checkbox" className="form-check-input" id="check1" name="cuisines" value="American" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check1" name="cuisines" value="American" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check1" name="cuisines" value="American" /> }
                                     <label className="form-check-label" htmlFor="check1">American</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Chinese") ?
-                                    <input type="checkbox" className="form-check-input" id="check2" name="cuisines" value="Chinese" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check2" name="cuisines" value="Chinese" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check2" name="cuisines" value="Chinese" /> }
                                     <label className="form-check-label" htmlFor="check2">Chinese</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Fast-Food") ?
-                                    <input type="checkbox" className="form-check-input" id="check3" name="cuisines" value="Fast-Food" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check3" name="cuisines" value="Fast-Food" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check3" name="cuisines" value="Fast-Food" /> }
                                     <label className="form-check-label" htmlFor="check3">Fast-Food</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Italian") ?
-                                    <input type="checkbox" className="form-check-input" id="check4" name="cuisines" value="Italian" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check4" name="cuisines" value="Italian" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check4" name="cuisines" value="Italian" />}
                                     <label className="form-check-label" htmlFor="check4">Italian</label>
                                 </div>
@@ -110,25 +110,25 @@ function edit_form( {place, id} ){
                             <div className="row m-auto">
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Sushi") ?
-                                    <input type="checkbox" className="form-check-input" id="check5" name="cuisines" value="Sushi" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check5" name="cuisines" value="Sushi" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check5" name="cuisines" value="Sushi" />}
                                     <label className="form-check-label" htmlFor="check5">Sushi</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Thai") ?
-                                    <input type="checkbox" className="form-check-input" id="check6" name="cuisines" value="Thai" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check6" name="cuisines" value="Thai" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check6" name="cuisines" value="Thai" />}
                                     <label className="form-check-label" htmlFor="check6">Thai</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Mexican") ?
-                                    <input type="checkbox" className="form-check-input" id="check7" name="cuisines" value="Mexican" checked /> : 
+                                    <input type="checkbox" className="form-check-input" id="check7" name="cuisines" value="Mexican" defaultChecked /> : 
                                     <input type="checkbox" className="form-check-input" id="check7" name="cuisines" value="Mexican" />}
                                     <label className="form-check-label" htmlFor="check7">Mexican</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("German") ?
-                                    <input type="checkbox" className="form-check-input" id="check8" name="cuisines" value="German" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check8" name="cuisines" value="German" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check8" name="cuisines" value="German" />}
                                     <label className="form-check-label" htmlFor="check8">German</label>
                                 </div>
@@ -136,25 +136,25 @@ function edit_form( {place, id} ){
                             <div className="row m-auto">
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Greek") ?
-                                    <input type="checkbox" className="form-check-input" id="check9" name="cuisines" value="Greek" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check9" name="cuisines" value="Greek" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check9" name="cuisines" value="Greek" />}
                                     <label className="form-check-label" htmlFor="check9">Greek</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Comfort") ?
-                                    <input type="checkbox" className="form-check-input" id="check10" name="cuisines" value="Comfort" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check10" name="cuisines" value="Comfort" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check10" name="cuisines" value="Comfort" />}
                                     <label className="form-check-label" htmlFor="check10">Comfort</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Indian") ?
-                                    <input type="checkbox" className="form-check-input" id="check11" name="cuisines" value="Indian" checked /> :
+                                    <input type="checkbox" className="form-check-input" id="check11" name="cuisines" value="Indian" defaultChecked /> :
                                     <input type="checkbox" className="form-check-input" id="check11" name="cuisines" value="Indian" />}
                                     <label className="form-check-label" htmlFor="check11">Indian</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("French") ?
-                                    <input type="checkbox" className="form-check-input" id="check12" name="cuisines" value="French" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check12" name="cuisines" value="French" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check12" name="cuisines" value="French" />}
                                     <label className="form-check-label" htmlFor="check12">French</label>
                                 </div>
@@ -162,25 +162,25 @@ function edit_form( {place, id} ){
                             <div className="row m-auto">
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Pizza") ?
-                                    <input type="checkbox" className="form-check-input" id="check13" name="cuisines" value="Pizza" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check13" name="cuisines" value="Pizza" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check13" name="cuisines" value="Pizza" />}
                                     <label className="form-check-label" htmlFor="check13">Pizza</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Mediterranean") ?
-                                    <input type="checkbox" className="form-check-input" id="check14" name="cuisines" value="Mediterranean" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check14" name="cuisines" value="Mediterranean" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check14" name="cuisines" value="Mediterranean" />}
                                     <label className="form-check-label" htmlFor="check14">Mediterranean</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Steakhouse") ?
-                                    <input type="checkbox" className="form-check-input" id="check15" name="cuisines" value="Steakhouse" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check15" name="cuisines" value="Steakhouse" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check15" name="cuisines" value="Steakhouse" />}
                                     <label className="form-check-label" htmlFor="check15">Steakhouse</label>
                                 </div>
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Vegetarian/Vegan") ?
-                                    <input type="checkbox" className="form-check-input" id="check16" name="cuisines" value="Vegetarian/Vegan" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check16" name="cuisines" value="Vegetarian/Vegan" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check16" name="cuisines" value="Vegetarian/Vegan" />}
                                     <label className="form-check-label" htmlFor="check16">Vegetarian/Vegan</label>
                                 </div>
@@ -188,7 +188,7 @@ function edit_form( {place, id} ){
                             <div className="row m-auto">
                                 <div className="form-check col-sm-3">
                                     { cuisinesChecked.includes("Other") ?
-                                    <input type="checkbox" className="form-check-input" id="check17" name="cuisines" value="Other" checked />:
+                                    <input type="checkbox" className="form-check-input" id="check17" name="cuisines" value="Other" defaultChecked />:
                                     <input type="checkbox" className="form-check-input" id="check17" name="cuisines" value="Other" />}
                                     <label className="form-check-label" htmlFor="check17">Other</label>
                                 </div>

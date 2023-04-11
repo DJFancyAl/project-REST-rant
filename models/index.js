7b370397c4526738967061049239838823ece7aa
DJFancyAl <abluemleiv@gmail.com>
2023-04-10 20:01:00 -0400

    CHanged to work with render

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://abluemleiv:lJGbyrNuWhjnfJtS@cluster0.wfuup0h.mongodb.net/restrant', {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
